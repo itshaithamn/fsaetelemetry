85d97905f29ce9f8e81dc530e774413bf90fb930
itshaithamn <hnaksho@oakland.edu>
2024-07-19 12:39:58 -0400

    tested using threads for a live data feed

--- src/main/java/org/main/chartviewController.java
+++ src/main/java/org/main/chartviewController.java
@@ -1,5 +1,6 @@
 package org.main;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -18,6 +19,8 @@ public class chartviewController{
     @FXML
     private NumberAxis yAxis;
 
+    private Thread chartThread;
+    private double timeInSeconds = 0.0;
 
     public void func(double [] globalArray) {
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
@@ -26,10 +29,34 @@ public class chartviewController{
         lineChart.setCreateSymbols(false);
         lineChart.setLegendVisible(false);
 
-        for (int i = 0; i < globalArray.length; i++) {
-            series.getData().add(new XYChart.Data<>(i, globalArray[i]));
-        }
+//        for (int i = 0; i < globalArray.length; i++) {
+//            series.getData().add(new XYChart.Data<>(i, globalArray[i]));
+//        }
+
+
+        chartThread = new Thread(() -> {
+            for (double data : globalArray) {
+                try {
+                    Thread.sleep(10); // Simulate real-time data feed every 0.01 seconds
+//                    System.out.println(timeInSeconds);
+                    Platform.runLater(() ->
+                            series.getData().add(new XYChart.Data<>(timeInSeconds, data)));
+                    timeInSeconds += 0.01;
+                } catch (InterruptedException e) {
+                    stopThread();
+                    return;
+                }
+            }
+        });
+        chartThread.start();
 
         lineChart.getData().add(series);
+    }
+
+    private boolean running = true;
+
+    private void stopThread() {
+        running = false; // Signal the thread to stop
+        chartThread.interrupt(); // Interrupt if sleeping or blocked
     }
 }
