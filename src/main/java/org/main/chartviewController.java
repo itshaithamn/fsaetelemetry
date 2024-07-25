83e1aef36a010569cc0e0e44e6f3af8bd0f1ff38
Matthew Hait <mhait@oakland.edu>
2024-07-25 12:55:08 -0400

    added a zoom plugin, breaks the chart because it breaks the update process

--- src/main/java/org/main/chartviewController.java
+++ src/main/java/org/main/chartviewController.java
@@ -3,9 +3,13 @@ package org.main;
 import io.fair_acc.chartfx.XYChart;
 import io.fair_acc.chartfx.axes.spi.DefaultNumericAxis;
 import io.fair_acc.dataset.spi.DoubleDataSet;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -37,13 +41,25 @@ public class chartviewController implements Initializable {
         xAxis.setAutoRanging(true);
 
         chart.getDatasets().add(dataSet);
-
+//        chart.getPlugins().add(new Zoomer());
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(this::updateCurrentTime, 0, 1, TimeUnit.MILLISECONDS);
     }
 
     public void collectdata(double[] globalArray) {
         this.globalArray = globalArray;
+    }
+
+    public void animation(){
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE);
+
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(100), actionEvent -> {
+           double lowerBound = dataSet.getDimension();
+           System.out.println(lowerBound);
+        });
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
     }
 
     public void collecttitle(String title){
