4fb35fd773ee1b41706b1f33a21b57690eeb8dd5
Matthew Hait <mhait@oakland.edu>
2024-07-26 22:24:26 -0400

    zoom in function doesnt work as an executable because of a font issue

---
+++ src/main/java/org/main/exec.java
@@ -0,0 +1,42 @@
+package org.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    public void chartsController(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/chartviewselection.fxml"));
+
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void videorenderController(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/videoselection.fxml"));
+
+        Parent root = fxmlLoader.load();
+        videorenderController controller = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void start(Stage stage) throws Exception {
+        videorenderController(new Stage());
+        chartsController(new Stage());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
